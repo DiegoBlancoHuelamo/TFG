ceba3b4650e108854ccfe11250c2575821859543
DiegoBlancoHuelamo <114072955+DiegoBlancoHuelamo@users.noreply.github.com>
2024-12-13 17:52:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template
 from subaplicaciones.actividadesMadrid import actividades_bp
 from subaplicaciones.culturaEducacion import cultura_bp
@@ -19,5 +18,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
